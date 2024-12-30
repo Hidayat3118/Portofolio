63a56113780ddb8f619f35c410538950cc6ef895
Hidayat3118 <muhammadhidayat3118@gmail.com>
2024-12-30 14:19:54 +0800

    tambaj faq

--- public/js/personal.js
+++ public/js/personal.js
@@ -16,3 +16,22 @@
     navLinks.classList.toggle('top-[10%]'); // Menampilkan navbar di atas
     navLinks.classList.toggle('top-[-100%]'); // Menyembunyikan navbar
   }
+
+   // Toggle FAQ content visibility
+   document.querySelectorAll('button').forEach(button => {
+    button.addEventListener('click', () => {
+      const content = document.getElementById(button.id + '-content');
+      const icon = document.getElementById(button.id + '-icon');
+      
+      if (content.classList.contains('max-h-0')) {
+        content.classList.remove('max-h-0');
+        content.classList.add('max-h-96'); // Set max-height to show content
+      } else {
+        content.classList.add('max-h-0');
+        content.classList.remove('max-h-96'); // Set max-height to collapse content
+      }
+
+      icon.classList.toggle('transform');
+      icon.classList.toggle('rotate-180');
+    });
+  });
