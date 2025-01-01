ec75eab736e58a57e0c212b17ca4a23ab6fc5ad0
Hidayat3118 <muhammadhidayat3118@gmail.com>
2025-01-01 21:15:00 +0800

    tambah animasi

--- public/js/personal.js
+++ public/js/personal.js
@@ -1,15 +1,10 @@
 // Js Dark Mode
-  // Pilih elemen input checkbox
   const toggleDarkMode = document.getElementById('toggle');
-
-  // Tambahkan event listener ke input
   toggleDarkMode.addEventListener('change', () => {
-    // Toggle kelas dark di elemen <html>
     document.documentElement.classList.toggle('dark');
   });
 
 // Js Navbar
-
   const navLinks = document.querySelector('.nav-links');
   function onToggleMenu(e) {
     e.name = e.name === 'menu' ? 'close' : 'menu';
@@ -35,3 +30,7 @@
       icon.classList.toggle('rotate-180');
     });
   });
+
+
+  // AOS Animation
+  AOS.init();
