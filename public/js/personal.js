43901b6c9416b4e04096e4270ad0c061b1076558
Hidayat3118 <muhammadhidayat3118@gmail.com>
2025-01-06 20:46:18 +0800

    perbaikan

--- public/js/personal.js
+++ public/js/personal.js
@@ -1,36 +1,52 @@
-// Js Dark Mode
-  const toggleDarkMode = document.getElementById('toggle');
-  toggleDarkMode.addEventListener('change', () => {
-    document.documentElement.classList.toggle('dark');
-  });
 
-// Js Navbar
-  const navLinks = document.querySelector('.nav-links');
-  function onToggleMenu(e) {
-    e.name = e.name === 'menu' ? 'close' : 'menu';
-    navLinks.classList.toggle('top-[10%]'); // Menampilkan navbar di atas
-    navLinks.classList.toggle('top-[-100%]'); // Menyembunyikan navbar
-  }
+            // Js Dark Mode
+            const toggleDarkMode = document.getElementById('toggle');
+            toggleDarkMode.addEventListener('change', () => {
+              document.documentElement.classList.toggle('dark');
+            });
 
-   // Toggle FAQ content visibility
-   document.querySelectorAll('button').forEach(button => {
-    button.addEventListener('click', () => {
-      const content = document.getElementById(button.id + '-content');
-      const icon = document.getElementById(button.id + '-icon');
-      
-      if (content.classList.contains('max-h-0')) {
-        content.classList.remove('max-h-0');
-        content.classList.add('max-h-96'); // Set max-height to show content
-      } else {
-        content.classList.add('max-h-0');
-        content.classList.remove('max-h-96'); // Set max-height to collapse content
-      }
+            // Js Navbar
+            const navLinks = document.querySelector('.nav-links');
+            function onToggleMenu(e) {
+              e.name = e.name === 'menu' ? 'close' : 'menu';
+              navLinks.classList.toggle('top-[10%]');
+              navLinks.classList.toggle('top-[-100%]');
+            }
+            // Toggle FAQ content visibility
+            document.querySelectorAll('button').forEach(button => {
+              button.addEventListener('click', () => {
+                const content = document.getElementById(button.id + '-content');
+                const icon = document.getElementById(button.id + '-icon');
 
-      icon.classList.toggle('transform');
-      icon.classList.toggle('rotate-180');
-    });
-  });
+                if (content.classList.contains('max-h-0')) {
+                  content.classList.remove('max-h-0');
+                  content.classList.add('max-h-96');
+                } else {
+                  content.classList.add('max-h-0');
+                  content.classList.remove('max-h-96');
+                }
+
+                icon.classList.toggle('transform');
+                icon.classList.toggle('rotate-180');
+              });
+            });
+            //  Line Clamp
+            document.addEventListener('DOMContentLoaded', () => {
+              function lineClamp(id) {
+                const line = document.getElementById(id);
+                line.classList.toggle('line-clamp-3');
+                line.classList.toggle('line-full');
+              }
+              // Project Satu
+              const lineSatu = document.getElementById('lineSatu');
+              lineSatu.addEventListener('click', () => lineClamp('lineSatu'));
+            
+              // Project Dua
+              const lineDua = document.getElementById('lineDua');
+              lineDua.addEventListener('click', () => lineClamp('lineDua'));
+            });
+            
 
 
-  // AOS Animation
-  AOS.init();
+         
+          
